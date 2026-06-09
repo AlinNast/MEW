db01fca0c5945d7fb3da2b1962994a32c5bd3246
AlinNast <alintudor.nast@gmail.com>
2026-06-09 11:28:17 +0200

    the engine opens a window

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
-#include < GLFW/glfw3.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main()
@@ -9,6 +10,10 @@ int main()
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
 	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create a windowed mode window and its OpenGL context
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
@@ -24,10 +29,19 @@ int main()
 	// Make the window's context current
     glfwMakeContextCurrent(window);
 
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
 	// Main loop
     while (!glfwWindowShouldClose(window))
     {
 		// Render here
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
 		// Swap front and back buffers
