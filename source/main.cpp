e892cda2223215c2a8956d4154abbc5a186dfdb9
AlinNast <alintudor.nast@gmail.com>
2026-06-09 11:15:33 +0200

    setup the right file structure

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,37 @@
+#include < GLFW/glfw3.h>
+#include <iostream>
+
 int main()
 {
+    if(!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
+
+	if (window == nullptr)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+
+
     return 0;
 }
